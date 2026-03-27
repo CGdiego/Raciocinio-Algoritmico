c19fe92ca90ad79c0ac206e682251ea3c0b8c2d4
CGdiego <266080606+CGdiego@users.noreply.github.com>
2026-03-27 12:12:25 -0300

    Modify files

--- Semana 5/Aula/Exercício 4.py
+++
@@ -1,9 +0,0 @@
-palavra = input("Digite uma palavra:\n")
-tamanho = len(palavra)
-
-while tamanho < 3 or tamanho > 10:
-    print("A palavra deve ter no mínimo 3 letras e no máximo 10.")
-    numero = (input("Palavra válida:\n"))
-
-print(palavra)
-print(tamanho)

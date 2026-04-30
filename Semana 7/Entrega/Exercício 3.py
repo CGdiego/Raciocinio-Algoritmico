f96714a4266a0c4c866ee9ed3d934ae83f768ec1
Diego <diego.fobs@gmail.com>
2026-04-29 22:17:51 -0300

    Another tiny adjustement

--- Semana 7/Entrega/Exercício 3.py
+++ Semana 7/Entrega/Exercício 3.py
@@ -5,4 +5,4 @@ for i in range(n1,n2+1): # Seleciona os números da tabuada
     print(f"Tabuada do {i}:") # Só pra deixar mais ajeitado com o título
     for tabuada in range(1, 11): # Vai de 1 até 10
         print(i * tabuada) # Multiplica o número da tabuada pelo multiplicador
-    print("\n") # Separa as linhas
+    print() # Separa as linhas

6c9d344bfd57331c0e811f2b6de758625f84d202
CGdiego <266080606+CGdiego@users.noreply.github.com>
2026-05-07 13:19:33 -0300

    Fixed folders order

--- Semana 5/Entrega/Exercício 1.py
+++
@@ -1,9 +0,0 @@
-nota = float(input("Insira sua nota:\n"))
-
-print("\n")
-
-while nota < 0 or nota > 10:
-    nota = float(input("Insira uma nota válida:\n"))
-    print("\n")
-else:
-    print("Tudo certo!")

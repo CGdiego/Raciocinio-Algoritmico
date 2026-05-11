69bc33a3e47c0022f14368305d6e8520e10c54af
CGdiego <266080606+CGdiego@users.noreply.github.com>
2026-05-10 21:54:56 -0300

    Finished the challenge

--- Semana 11/Aula/Vetor/Exercício 5 (Desafio).py
+++ Semana 11/Aula/Vetor/Exercício 5 (Desafio).py
@@ -1,16 +1,22 @@
 vetor = [0, 0, 0, 0, 0, 0]
-sena = [9, 13, 21, 32, 33, 59]
+sena = [9, 13, 21, 32, 33, 59] # Valores da última Mega da Virada
+acertos = 0
 
 for i in range(6):
     aposta = input(f"Insira o seu {i+1}º número para a Mega-Sena: ")
 
     # Enquanto não for número
-    while not aposta.isdigit():
+    while not aposta.isdigit() or int(aposta) > 60 or int(aposta) < 1 or int(aposta) in vetor:
+        print("\nValor inválido, digite novamente.")
         aposta = input(f"Insira o seu {i+1}º número para a Mega-Sena: ")
 
     aposta = int(aposta)
     vetor[i] = aposta
 
-    if vetor[i] == sena[i]:
+    for s in sena:
+        if vetor[i] == s:
+            acertos += 1
 
-print(vetor)
+print(f"\nNúmeros sorteados: {sena}.")
+print(f"Números jogados: {vetor}.")
+print(f"Quantidade de acertos: {acertos}/6.")

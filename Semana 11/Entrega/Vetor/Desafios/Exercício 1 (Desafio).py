dc24b5ec62b9134ca4913e8b98c60940c346d90c
CGdiego <266080606+CGdiego@users.noreply.github.com>
2026-05-14 09:39:08 -0300

    Added new comments on the code

--- Semana 11/Entrega/Vetor/Desafios/Exercício 1 (Desafio).py
+++ Semana 11/Entrega/Vetor/Desafios/Exercício 1 (Desafio).py
@@ -4,13 +4,13 @@ i = 0
 
 while i < len(valores):
     repetido = False # Não há nenhum valor repetido no início
-    ii = 0
+    ii = 0 # Novo índice
     while ii < i:
         if valores[i] == valores[ii]:
             repetido = True
         ii += 1
     if repetido:
-        valores[i] = -999
+        valores[i] = -999 # Seta o valor repitido pra -999
     i += 1
 
 i = 0
